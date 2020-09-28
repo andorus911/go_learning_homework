a2bacedcd23b19485b8258da2dc8e1c0fbc07ac1
Andrey Loginov <andorus911@gmail.com>
2020-09-28 16:50:37 +0300

    FIX: ReadDir()

--- 12_working_with_os/go-envdir/main.go
+++ 12_working_with_os/go-envdir/main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
-	"strings"
+	"path/filepath"
 )
 
 func main() {
-
+	
 }
 
 func ReadDir(dir string) (map[string]string, error) {
-	err := os.Chdir(dir)
+	filesFromDir := make(map[string]string)
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		mode := info.Mode()
+		if mode.IsRegular() {
+			b, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			filesFromDir[path] = string(b)
+		}
+		return nil
+	})
+
 	if err != nil {
 		return nil, err
 	}
-	env := os.Environ()
-	envMap := make(map[string]string)
-	for _, v := range env {
-		envVariable := strings.Split(v, "=")
-		envMap[envVariable[0]] = envVariable[1]
-	}
-	return envMap, nil
+	return filesFromDir, nil
 }
 
 //func RunCmd(cmd []string, env map[string]string) int {}
