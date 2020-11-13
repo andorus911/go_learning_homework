a4d33e1c48ac275727f2a0eb9fd9992874701c1d
Andrey Loginov <andorus911@gmail.com>
2020-11-13 13:01:17 +0300

    ADD: create event method

--- go-calendar-ms/api/main.go
+++ go-calendar-ms/api/main.go
@@ -1,25 +1,40 @@
 package api
 
 import (
+	"context"
+	"encoding/json"
 	"go.uber.org/zap"
-	"log"
+	"go_learning_homework/go-calendar-ms/internal/domain/services"
 	"net/http"
+	"strconv"
+	"time"
 )
 
-var lg zap.Logger
+const LAYOUT = "2006.01.02 15:04:05"
 
-func StartServer(httpListen string, logger zap.Logger) {
+var lg zap.Logger
+var evService services.EventService
+
+func StartServer(httpListen string, logger zap.Logger, eventService *services.EventService) {
 	lg = logger
+	evService = *eventService
+
+	evh := &EventHandler{}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/event/create", evh.create) // timeout?
+	mux.HandleFunc("/event/update", evh.update)
+	mux.HandleFunc("/event/delete", evh.delete)
+	mux.HandleFunc("/event/myList", evh.getList)
 
 	server := http.Server{
 		Addr: httpListen,
-		Handler: nil, // TODO
+		Handler: mux,
 	}
 
 	//go func() {
 		err := server.ListenAndServe()
 		if err != nil {
-			log.Fatal(err)
+			lg.Fatal(err.Error())
 		}
 	//}()
 }
@@ -27,18 +42,61 @@ func StartServer(httpListen string, logger zap.Logger) {
 type EventHandler struct{}
 
 func (h *EventHandler) create(w http.ResponseWriter, r *http.Request) {
-	//r.PostFormValue()
-	//services.EventService{}
+	if r.Method != "POST" {
+		lg.Warn("incorrect method")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		_, err := w.Write([]byte("Allow: POST"))
+		if err != nil {
+			lg.Error("response writer error: " + err.Error())
+		}
+		return
+	}
+
+	title := r.PostFormValue("title")
+	if title == "" {
+		lg.Warn("incorrect title")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	description := r.PostFormValue("description")
+	owner, err := strconv.ParseInt(r.PostFormValue("owner"), 10, 64)
+	if err != nil {
+		lg.Warn("incorrect owner: " + err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	startTime, err := time.Parse(LAYOUT, r.PostFormValue("startTime"))
+	if err != nil {
+		lg.Warn("incorrect startTime: " + err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	endTime, err := time.Parse(LAYOUT, r.PostFormValue("endTime"))
+	if err != nil {
+		lg.Warn("incorrect endTime: " + err.Error())
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 2 * time.Second)
+	event, err := evService.CreateEvent(ctx, title, description, owner, startTime, endTime)
+	if err != nil {
+		lg.Error("event creating error: " + err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	out, err := json.Marshal(event)
+	_, err = w.Write(out)
+	if err != nil {
+		lg.Error("response writer error: " + err.Error())
+	}
+	return
 }
 
-func (h *EventHandler) update(w http.ResponseWriter, r http.Request) {
+func (h *EventHandler) update(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (h *EventHandler) delete(w http.ResponseWriter, r http.Request) {
+func (h *EventHandler) delete(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (h *EventHandler) getList(w http.ResponseWriter, r http.Request) {
+func (h *EventHandler) getList(w http.ResponseWriter, r *http.Request) {
 
 }
